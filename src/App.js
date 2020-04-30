c27596b6877c51c7adeb7761da3c7474672c14e3
Pawel Borowik <pawelbor1@op.pl>
2020-04-30 17:43:12 +0200

    add style.commponets to add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddBook from "./AddBook";
 import {useSelector} from "react-redux";
+import styled from "styled-components"
 import Book from "./Book";
 
 import './App.css';
@@ -11,20 +12,32 @@ function App() {
 // const [books, setBooks]= useState([]) przed dodaniem reduxa
 const books= useSelector(state => state.books)
 
+const Title = styled.h1`
+  font-size: 1.7em;
+  text-align: center;
+  color: #000;
+`;
+
+
+const Wrapper = styled.header`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 
 
 
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>Ulubione Ksiązki</h1>
+      <Wrapper>
+        <Title>
+              Ulubione Ksiązki
+        </Title>
+            
+      </Wrapper>
       {/* /<AddBook books={books} setBooks={setBooks}/> */}
       <AddBook />
       <Book books={books}/>
-  
-      </header>
-
-
     </div>
   );
 }
